90cbb2fa22e783cb2aa5b477a836ec8ba7dbd682
Bárbara Gabriela <barbara.grv27@gmail.com>
2024-11-12 15:06:38 -0300

    inicio do projeto

---
+++ Calculadora4/src/main/java/com/mycompany/calculadora4/Calculadora4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora4;
+
+/**
+ *
+ * @author 70114083630
+ */
+public class Calculadora4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
